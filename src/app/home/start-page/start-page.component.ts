7b80730f731e2c9b4bfc1ad368750030677d8326
Nico Orozco <niko.orozc@gmail.com>
2024-07-05 23:29:28 -0500

    se crea json base para la strat page, se crea start page con funcionalidad para redirigir al modulo correspondiente. se deja start page lista para agregar iconos y urls de las vistas

--- src/app/home/start-page/start-page.component.ts
+++ src/app/home/start-page/start-page.component.ts
@@ -1,23 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import StartPage from 'src/assets/jsons-base/start-page.json';
 
 @Component({
   selector: 'app-start-page',
   templateUrl: './start-page.component.html',
   styleUrls: ['./start-page.component.css']
 })
-export class StartPageComponent {
+export class StartPageComponent implements OnInit {
+
+  cards: any[] = [];
+  isFlipped: boolean = false;
 
   constructor(private router: Router) { }
 
-  redirigir(vista: string) {
-    switch (vista) {
-      case 'vista1':
-        this.router.navigate(['/home/crear-db']);
-        break;
-      case 'vista2':
-        this.router.navigate(['/home/crear-tabla']);
-        break;
-    }
+  public redirigir(vista: string) {
+    window.location.href = vista;
+  }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  public loadData(): void {
+    this.cards = StartPage.cards.map(card => ({ ...card, isFlipped: false }));
+  }
+
+  onMouseOver(index: number) {
+    this.cards[index].isFlipped = true;
+  }
+
+  onMouseOut(index: number) {
+    this.cards[index].isFlipped = false;
   }
 }
